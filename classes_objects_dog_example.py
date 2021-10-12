b04542f3c3e9d3656da82c5147a0a66cce9c424a
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-10-12 21:31:19 +0300

    played with empty list as class attribute

--- classes_objects_dog_example.py
+++ classes_objects_dog_example.py
@@ -20,8 +20,19 @@ class Dog:
     def set_age(self, age):
         self.age = age
 
+class Cat:
 
-# <editor-fold desc="print part">
+    def __init__(self, name):
+        self.name = name
+        self.nicks = []
+
+    def get_name(self):
+        return self.name
+
+    def set_nick(self, nick):
+        self.nicks.append(nick)
+
+# <editor-fold desc="print dog part">
 d1 = Dog('Tim', 32)
 print('d1 = Dog("Tim", 32) | ', d1)
 
@@ -40,6 +51,21 @@ print('d1.get_age() | ', d1.get_age())
 
 print('d1.bite_more(2) | ', d1.bite_more(2))
 # </editor-fold>
+print('\n')
+# <editor-fold desc="print dog part">
+c1 = Cat('Vin')
+print('c1 = Cat("Vin") | ', c1)
+print('c1.get_name() | ', c1.get_name())
+print('c1.name | ', c1.name)
+print('c1.get_name() | ', c1.nicks)
+print('c1.set_nick() | ', c1.set_nick('zhyr'))
+print('c1.get_name() | ', c1.nicks)
+print('c1.set_nick() | ', c1.set_nick('salo'))
+print('c1.get_name() | ', c1.nicks)
+
+# </editor-fold>
+
+
 
 
 
