ba03f6fc3fd26a05bfc0293901246bf3917310a1
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-10-12 21:03:43 +0300

    Update classes_objects_dog_example.py

--- classes_objects_dog_example.py
+++ classes_objects_dog_example.py
@@ -1,19 +1,18 @@
-
 class Dog:
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        print(name)
-
-    def get_name(self):
-        return self.name
-
-    def bite_more(self, x):
-        return x + 1
+        print(name, age)
 
     def bark(self):
         print('woof')
+
+    def bite_more(self, bites_num):
+        return 'bite ' * bites_num
+
+    def get_name(self):
+        return self.name
 
     def get_age(self):
         return self.age
@@ -22,57 +21,25 @@ class Dog:
         self.age = age
 
 
-print('d1', end=' ')
+# <editor-fold desc="print part">
 d1 = Dog('Tim', 32)
+print('d1 = Dog("Tim", 32) | ', d1)
 
-print('d1.name', end=' ')
-print(d1.name)
-
-print('d1.get_name()', end=' ')
-print(d1.get_name())
-
-print('d1.get_age()', end=' ')
-print(d1.get_age())
-
-print('d1.set_age(17)', end=' ')
-d1.set_age(17)
-
-print('d1.get_age()', end=' ')
-print(d1.get_age())
-
-print(d1.bite_more(2))
-
-"""
 d2 = Dog('Bill', 12)
-print(d2.name)
-print(d2.get_name())
-print(d2.get_age())
-"""
+print('d2 = Dog("Bill", 12) | ', d2)
 
-"""
-classes
-class Dog: 
-there can be millions of dogs but Dog class describe each of them
+print('d1.name | ', d1.name)
 
-methods
-def bark(self):
-    print('woof')
-methods are actions that can be done within a class,they are functions by nature
+print('d1.get_name() | ', d1.get_name())
 
-parameters
-(self) in bark method is method's parameter, there can be a lot of them
-self param. unlike others possible params says that method will be conducted on the called object itself
+print('d1.get_age() | ', d1.get_age())
 
-def __init__ ():
-if the class has __init__ method, the  method is automatically called when the object of this class is called
+print('d1.set_age(17) | ', d1.set_age(17))
 
-def __init__(self, name, age):
-    self.name = name
-    self.age = age
-    print(name)
-it also can store class attributes
-"""
+print('d1.get_age() | ', d1.get_age())
 
+print('d1.bite_more(2) | ', d1.bite_more(2))
+# </editor-fold>
 
 
 
