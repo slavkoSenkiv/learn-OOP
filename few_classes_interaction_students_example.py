456603d7571bc9690b27e8c8cd4a1e5377c95917
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-10-13 22:25:48 +0300

    Update few_classes_interaction_students_example.py

--- few_classes_interaction_students_example.py
+++ few_classes_interaction_students_example.py
@@ -2,45 +2,52 @@ class Student:
     def __init__(self, name, age, grade):
         self.name = name
         self.age = age
-        self.grade = grade + 100  # 0 - 100
-
-    def get_grade(self):
-        return self.grade
+        self.grade = grade
 
 
 class Course:
-
     def __init__(self, name, max_students):
         self.name = name
-        self.max_students = max_students
-        self.students = []
+        self.max_students_num = max_students
+        self.students_enrolled = []
 
     def add_student(self, student):
-        if len(self.students) < self.max_students:
-            self.students.append(student)
-            return True
-        return False
+        if len(self.students_enrolled) < self.max_students_num:
+            self.students_enrolled.append(student)
+        else:
+            print('too much students inside course already')
+            return False
 
-    def get_avarage_grade(self):
+    def get_average_grade(self):
         value = 0
-        for student in self.students:
-            value += student.get_grade()
+        for student in self.students_enrolled:
+            student_grade = student.grade
+            value += student_grade
+        average_grade = value / len(self.students_enrolled)
+        return average_grade
 
-        return value / len(self.students)
+
+def play_student(name1, age1, grade1):
+    s = Student(name1, age1, grade1)
+    # print(s.name)
+    # print(s.age)
+    # print(s.grade)
+    # print('\n')
+    return s
 
 
-s1 = Student('Tim', 19, 95)
-s2 = Student('Bill', 19, 75)
-s3 = Student('Jill', 19, 65)
+s1 = play_student('Tim', 20, 50)
+s2 = play_student('Bill', 21, 60)
 
-course = Course('Math', 2)
-course.add_student(s1)
-course.add_student(s2)
-
-print('course.add_student(s3)', course.add_student(s3))
-print('course.students', course.students)
-print('course.get_avarage_grade()', course.get_avarage_grade())
-print('s1.get_grade()', s1.get_grade())
-print('s1.grade', s1.grade)
-
+c = Course('Math', 2)
+print(c.name)
+print(c.max_students_num)
+print(c.students_enrolled)
+c.add_student(play_student('Tim', 20, 50))
+print(c.students_enrolled)
+c.add_student(s1)
+print(c.students_enrolled)
+c.add_student(s2)
+print(c.students_enrolled)
+print(c.get_average_grade())
 
