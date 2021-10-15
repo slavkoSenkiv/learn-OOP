3a982b20f66ff0b5a82b5e80c95e9b7937b7069f
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-10-15 08:47:59 +0300

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,17 +1,18 @@
 class Pet:
-    def __init__(self, name, age, color):
+    def __init__(self, type, name, age, color):
         self.name = name
+        self.type = type
         self.age = age
         self.color = color
 
-
-class Cat(Pet):
-    def __init__(self, name, age, color, fur):
-        super().__init__(name, age, color)
-        self.fur = fur
-
     def show(self):
         print(f"I'm {self.name} and I'm {self.age} years old and I'm {self.color}")
+
+
+class Cat(Pet):
+    def __init__(self, type, name, age, color, fur):
+        super().__init__(type, name, age, color)
+        self.fur = fur
 
     def speak(self):
         print('Meow')
@@ -22,28 +23,25 @@ class Dog(Pet):
         print('Bark')
 
 
-c = Cat('Tim', 7, 'black', 'long')
-d = Dog('Bill', 9, 'brown')
+c = Cat('cat', 'Tim', 7, 'black', 'long')
+d = Dog('dog', 'Bil', 9, 'brown')
 
 
 def play_obj(class1):
-    print(class1.name)
-    print(class1.age)
-    print(class1.color)
-    class1.speak()
+    print('type', class1.type)
+    print('name', class1.name)
+    print('age', class1.age)
+    print('color', class1.color)
+    print('.speak()', end=' '); class1.speak()
+    print('.show()', end=' '); class1.show()
     print('\n')
 
 
 play_obj(c)
 play_obj(d)
-
 print(c.fur)
 
-<<<<<<< Updated upstream
-=======
-c = Cat('Bill', 34, 'black')  # , 'black')
-c.show()
-c.speak()
->>>>>>> Stashed changes
+
+
 
 
